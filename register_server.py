292a1de02a03a084cc47e0083cb41e187929fca6
AndreiRO <andysstef@yahoo.com>
2015-07-22 22:16:11 +0300

    Working client for non-Arduino boards

--- register_server.py
+++ register_server.py
@@ -4,7 +4,7 @@ import requests
 import json
 import time
 
-from threading import Thread, Lock, Timer
+from threading import Thread, Lock
 
 from wyliodrin import *
 
@@ -33,16 +33,27 @@ class Sensor(object):
 
 
     def get(self, url, token):
+        url = url + ('/' if url[-1] != '/' else '') + 'get'
         while True:     
             try:
                 self.lock.acquire()
-                url = url + ('/' if url[-1] != '/' else '') + 'get'
+                
                 data = {"id" : self._id, "token" : token}
                 res = requests.post(url, data=data)
 
                 if res.status_code == 200:
                     obj = json.loads(res.content)
-                    self.value = obj["value"]
+
+                    if obj["error"] != 0:
+                        print obj["reason"]
+                    else:
+                        self.value = obj["value"]
+                        print "Got value: ", self.value, " for sensor on pin: ", self.pin
+
+                        if self._type == DIGITAL_OUTPUT:
+                            digitalWrite(self.pin, self.value)
+                        elif self._type == PWM_OUTPUT:
+                            analogWrite(self.pin, self.value)
                 else:
                     print "Error: " + res.reason
 
@@ -53,14 +64,14 @@ class Sensor(object):
 
 
     def send(self, url, token):
+        url = url + ('/' if url[-1] != '/' else '') + 'get'
         while True:
             try:
                 self.lock.acquire()
-                url = url + ('/' if url[-1] != '/' else '') + 'get'
                 
                 if self._type == DIGITAL_INPUT:
                     self.value = digitalRead(self.pin)
-                else:
+                elif self._type == ANALOG_INPUT:
                     self.value = analogRead(self.pin)
 
                 data = {"id" : self._id, "token" : token, "value" : self.value}
@@ -68,6 +79,8 @@ class Sensor(object):
 
                 if res.status_code != 200:
                     print "Error: " + res.reason
+                else:
+                    print "Sent value: ", self.value, " for sensor on pin: ", self.pin
 
             finally:
                 self.lock.release()
@@ -95,7 +108,94 @@ class RegisterServer(object):
         else:
             s = Sensor(_id, DIGITAL_INPUT, pin, sleep_time)
             self.sensors[_id] = {"object" : s, "autosend" : sleep_time}
-            s.thread = Thread(s.send, args=(self.url, self.token))
+    
+            url = self.url + ("/" if self.url[-1] != "/" else "") + "/register"
+            data = {"id" : _id, "token" : self.token, "type" : DIGITAL_INPUT}
+            res = requests.post(url, data=data)
+
+            if res.status_code != 200:
+                print "Error regitstering DIGITAL_INPUT on pin: ", pin, " Reason: ", res.reason
+                return False
+            
+            obj = json.loads(res.content)
+            if obj["error"] != 0 and obj["error"] != 2:
+                print "Error regitstering DIGITAL_INPUT on pin: ", pin, " Reason: ", obj["reason"]
+                return False
+
+            s.thread = Thread(target=s.send, args=(self.url, self.token))
             s.thread.start()
 
+    def registerAnalogInput(self, _id, pin, sleep_time):
 
+        if _id in self.sensors or not validate_pin(pin):
+            print "Id: ", _id, " already registerd"
+            return False
+        else:
+            s = Sensor(_id, ANALOG_INPUT, pin, sleep_time)
+            self.sensors[_id] = {"object" : s, "autosend" : sleep_time}
+
+            url = self.url + ("/" if self.url[-1] != "/" else "") + "/register"
+            data = {"id" : _id, "token" : self.token, "type" : ANALOG_INPUT}
+            res = requests.post(url, data=data)
+
+            if res.status_code != 200:
+                print "Error regitstering ANALOG_INPUT on pin: ", pin, " Reason: ", res.reason
+                return False
+            
+            obj = json.loads(res.content)
+            if obj["error"] != 0 and obj["error"] != 2:
+                print "Error regitstering ANALOG_INPUT on pin: ", pin, " Reason: ", obj["reason"]
+                return False
+
+            s.thread = Thread(target=s.send, args=(self.url, self.token))
+            s.thread.start()
+
+    def registerDigitalOutput(self, _id, pin, sleep_time):
+
+        if _id in self.sensors or not validate_pin(pin):
+            print "Id: ", _id, " already registerd"
+            return False
+        else:
+            s = Sensor(_id, DIGITAL_OUTPUT, pin, sleep_time)
+            self.sensors[_id] = {"object" : s, "autosend" : sleep_time}
+
+            url = self.url + ("/" if self.url[-1] != "/" else "") + "/register"
+            data = {"id" : _id, "token" : self.token, "type" : DIGITAL_OUTPUT}
+            res = requests.post(url, data=data)
+
+            if res.status_code != 200:
+                print "Error regitstering DIGITAL_OUTPUT on pin: ", pin, " Reason: ", res.reason
+                return False
+            
+            obj = json.loads(res.content)
+            if obj["error"] != 0 and obj["error"] != 2:
+                print "Error regitstering DIGITAL_OUTPUT on pin: ", pin, " Reason: ", obj["reason"]
+                return False
+
+            s.thread = Thread(target=s.get, args=(self.url, self.token))
+            s.thread.start()
+
+    def registerPWMOutput(self, _id, pin, sleep_time):
+
+        if _id in self.sensors or not validate_pin(pin):
+            print "Id: ", _id, " already registerd"
+            return False
+        else:
+            s = Sensor(_id, PWM_OUTPUT, pin, sleep_time)
+            self.sensors[_id] = {"object" : s, "autosend" : sleep_time}
+
+            url = self.url + ("/" if self.url[-1] != "/" else "") + "/register"
+            data = {"id" : _id, "token" : self.token, "type" : PWM_OUTPUT}
+            res = requests.post(url, data=data)
+
+            if res.status_code != 200:
+                print "Error regitstering PWM_OUTPUT on pin: ", pin, " Reason: ", res.reason
+                return False
+            
+            obj = json.loads(res.content)
+            if obj["error"] != 0 and obj["error"] != 2:
+                print "Error regitstering PWM_OUTPUT on pin: ", pin, " Reason: ", obj["reason"]
+                return False
+
+            s.thread = Thread(target=s.get, args=(self.url, self.token))
+            s.thread.start()
